423e0651e7d6dab610d071d373ec85d5b120e4d3
Administrator <caiqw@hxcfsoft.com>
2018-04-09 10:18:54 +0800

    client类名改为Application

--- src/main/java/com/study/crawler/client/Application.java
+++ src/main/java/com/study/crawler/client/Application.java
@@ -8,9 +8,9 @@ import com.study.crawler.factory.CrawlerFactory;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.study.crawler" }) // 用来自动扫描 该路径下的注解
-public class client {
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(client.class, args);
+		SpringApplication.run(Application.class, args);
 		CrawlerFactory factory = ApplicationContextSave.getBean(CrawlerFactory.class);
 		factory.startCrawler();
 		System.exit(0);
